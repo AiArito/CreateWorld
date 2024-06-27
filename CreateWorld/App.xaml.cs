ef579825a7b4bdbbc96e1321b43cb61342656609
AiArito <akaringo4869@docomo.ne.jp>
2024-06-27 15:17:58 +0900

    プロジェクト ファイルを追加します。

---
+++ CreateWorld/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CreateWorld
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
